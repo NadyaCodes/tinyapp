0def2d076b28376a0130c8f0ee0151fd858608bd
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-18 16:21:09 -0400

    added body-parser and made function that creates shortened URLs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+// const res = require('express/lib/response');
 
 app.set("view engine", "ejs");
 
@@ -8,6 +11,16 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+
+function generateRandomString() {
+  let randomString = "";
+  const stringOptions = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  for (let i = 0; i < 6; i++) {
+    randomString += stringOptions[Math.floor(Math.random() * stringOptions.length)]
+  }
+  return randomString;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,21 +31,35 @@ app.listen(PORT, () => {
 });
 
 // THIS IS THE ONE I DON'T KNOW IF I NEED
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-})
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// })
 
 //THIS IS THE NEW ONE THAT I DON'T KNOW IF IT REPLACES ABOVE
+
+
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  req.body.shortURL = generateRandomString();
+  console.log(req.body)
+  res.send("Ok");
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
 
 
 app.get("/hello", (req, res) => {
