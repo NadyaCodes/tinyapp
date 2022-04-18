069b4babb7c9d432600e2c6d2c129c39411e9a1a
NadyaCodes <nadyacorscadden@gmail.com>
2022-04-18 14:56:14 -0400

    made and linked template for URL pages..urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -17,16 +17,23 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-//THIS IS THE ONE I DON'T KNOW IF I NEED
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// })
+// THIS IS THE ONE I DON'T KNOW IF I NEED
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
 
 //THIS IS THE NEW ONE THAT I DON'T KNOW IF IT REPLACES ABOVE
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
